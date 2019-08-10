4415536b11fec862c24dbe8301e03e42e28b25eb
Brenna Nieva <bnieva101@gmail.com>
2019-08-10 03:17:48 -0700

    social icons, Baymax AI integration init

---
+++ Save a Life Project/Save a Life ProjectTests/Save_a_Life_ProjectTests.swift
@@ -0,0 +1,34 @@
+//
+//  Save_a_Life_ProjectTests.swift
+//  Save a Life ProjectTests
+//
+//  Created by Brenna Nieva on 8/9/19.
+//  Copyright © 2019 Brenna Nieva. All rights reserved.
+//
+
+import XCTest
+@testable import Save_a_Life_Project
+
+class Save_a_Life_ProjectTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
